8949eca34702e4382127418e5be6cae28a621cea
AFcoder10 <adityafere.6@gmail.com>
2026-07-01 16:51:58 +0530

    refactor: reorganize asset directory and remove unused icon files and test script

--- test.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-# Open the PNG image
-img = Image.open('icon.png')
-
-# Save as ICO, automatically generating standard icon sizes
-img.save('your_icon.ico', format='ICO', sizes=[(256, 256), (128, 128), (64, 64), (32, 32), (16, 16)])
